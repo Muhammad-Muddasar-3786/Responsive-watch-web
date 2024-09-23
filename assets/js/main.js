a96e7d5e8c99de26e4c5a6a0d6e0b3734b27c2c2
your-MuhammadMuddasar <your-muhammadmuddasar3786@gmail.com>
2024-09-23 12:05:49 +0500

    All basic js done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,178 @@
 //--------SHOW MENU-----------
 
-const navMenu = document.querySelector('#nav-menu'),
-    navToggle = document.querySelector('#nav-toggle'),
-    navClose = document.querySelector('#nav-close');
-      
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
     //----Logic------
 
     //---- Show Menu-----------
-    if (navToggle) {
-        navToggle.addEventListener('click', () => {
-            navMenu.classList.add('showMenu')
+    if(navToggle){
+        navToggle.addEventListener('click', () =>{
+            navMenu.classList.add('show-menu')
         })
-        
     }
 
      //---- Hide Menu-----------
-     if (navToggle) {
-        navClose.addEventListener('click', () => {
-            navMenu.classList.remove('showMenu')
+     if(navClose){
+        navClose.addEventListener('click', () =>{
+            navMenu.classList.remove('show-menu')
         })
-        
-     }
+    }
     // -----END------
 
 
     //--------REMOVE MENU MOBILE-----------
-    const navLink = document.querySelectorAll('.nav-link')
+    const navLink = document.querySelectorAll('.nav__link')
 
     //-----Logic-------
-    const linkAction = () => {
-        navMenu = document.querySelector('#nav-menu')
-        navMenu.classList.remove('showMenu')
+    const linkAction = () =>{
+        const navMenu = document.getElementById('nav-menu')
+        // When we click on each nav__link, we remove the show-menu class
+        navMenu.classList.remove('show-menu')
 
     }
 
     navLink.forEach(n => n.addEventListener('click', linkAction))
     //-----END------
 
-   
+//-------------CHANGE BACKGROUNG HEADER----------------
+
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('scroll-header') 
+                       : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+//----------END---------------
+
+
+
+/*=============== TESTIMONIAL SWIPER ===============*/
+let testimonialSwiper = new Swiper(".testimonial-swiper", {
+    spaceBetween: 30,
+    loop: 'true',
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+/*=============== NEW SWIPER ===============*/
+let newSwiper = new Swiper(".new-swiper", {
+    spaceBetween: 24,
+    loop: 'true',
+
+    breakpoints: {
+        576: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 3,
+        },
+        1024: {
+          slidesPerView: 4,
+        },
+    },
+});
+
+
+//-----------SCROLL SECTION ACTIVE LINKS-------------
+
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
+
+//-----------SHOW SCROLL UP-----------
+
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+
+//----------END---------------
+
+
+
+//----------------CART SHOW--------------
+
+const cart = document.getElementById('cart'),
+      cartShop = document.getElementById('cart-shop'),
+      cartClose = document.getElementById('cart-close')
+
+
+      //----Logic------
+      //----------------Showing cart--------------
+
+if(cartShop){
+    cartShop.addEventListener('click', () =>{
+        cart.classList.add('show-cart')
+    })
+}
+
+//----------------Cart hidden--------------
+
+if(cartClose){
+    cartClose.addEventListener('click', () =>{
+        cart.classList.remove('show-cart')
+    })
+}
+
+//----------END---------------
+
+
+
+//----------------LIGHT/DARK MODE--------------
+
+const themeButton = document.getElementById('theme-button'),
+      darkTheme = 'dark-theme',
+      iconTheme = 'bx-sun'
+
+const selectedTheme = localStorage.getItem('selected-theme'),
+      selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme)
+    
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+
+})
+      
+  
+//----------END---------------
+
+// All basic JS is done
